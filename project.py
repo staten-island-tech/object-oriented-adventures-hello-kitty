41009f03423d08e7cc5b6a1acdcc4a519129654a
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-04-08 12:27:57 -0400

    Update project.py

--- project.py
+++ project.py
@@ -7,7 +7,15 @@
 # Locations - Forest
 
 
-# Introduction: Narrator: "All of your(Hello Kitty) friends had turned evil due to a illness. "
+# Introduction: Narrator: "All of your(Hello Kitty) friends have turned evil due to an unknown illness!
+#You need to memorize the paths that your friends are not on, in order to make it to the end of the forest."
+#At the end of the forest you will find a potion that turns all your friends back to normal."
+
 # Asks question: Do you want to go left or right?
+#First choice from the start: 
+
 # If the user goes right:
-#
+# Faces My Melody: Gets infected and dies. Print("Game Over") v
+#Goes back to the beginning of the game.
+#If the user chooses left:
+
