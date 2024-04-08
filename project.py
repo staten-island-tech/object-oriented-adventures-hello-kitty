8428583873d2020d70c5f4ddd2b7d32cdcc29e0a
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-04-08 12:14:04 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,13 @@
 # Hero - Hello Kitty
 # Enemy - Friends(original) 
+#Enemies (8): My Melody, Mimmy, Pompompurin, Keroppi, Kuromi, Tuxedo Sam, Pochacco, Gudetama.
 # Class Type for Hero - 
 # Merchant - 
 # Item - 
 # Locations - Forest
 
+
+# Introduction: Narrator: "All of your(Hello Kitty) friends had turned evil due to a illness. "
 # Asks question: Do you want to go left or right?
-# 
+# If the user goes right:
+#
