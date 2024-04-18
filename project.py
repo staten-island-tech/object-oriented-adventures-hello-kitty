63634df785c872d238ae173cc8cc7516786286e3
olamideo123 <olamideo12@nycstudents.net>
2024-04-18 12:29:21 -0400

    nn

--- project.py
+++ project.py
@@ -11,33 +11,4 @@
 #You need to memorize the paths that your friends are not on, in order to make it to the end of the forest."
 #At the end of the forest you will find a potion that turns all your friends back to normal."
 
-
-# Asks question1: Do you want to go left or right?
-#First choice from the start:
-
-# If the user goes right:
-# Faces My Melody: Gets infected and dies. Print("Game Over")
-#Goes back to the beginning of the game.
-
-#If the user chooses left: Print("Safe.") 
-
-#Asks question 2: "Do you want to go left or right?"
-
-
-
-#If user goes left: 
-# Faces Mimmy: Gets infected and dies. Print("Game Over")
-#Goes back to the beginning of the game.
-
-#If user goes right:Print("Safe.") 
-#Asks question 3: "Do you want to go left or right?"
-
-#If user goes right:faces Pompompurin: Gets infected and dies. Print("Game Over")
-#Goes back to the beginning of the game.
-
-#If the user goes left: Print("Safe")
-#Asks question 4: "Do you want to go left or right?"
-
-#If the user goes right: print("safe")
-#If the user goes left: 
-#Faces Keroppi: Gets infected and dies. Print("Game over.")
+ 
