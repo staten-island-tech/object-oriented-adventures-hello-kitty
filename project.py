e017b013f47381e4523956bb5f74f2fdd359a13a
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-05-01 12:14:10 -0400

    Update project.py

--- project.py
+++ project.py
@@ -3,9 +3,10 @@
 #Enemies (8): My Melody, Mimmy, Pompompurin, Keroppi, Kuromi, Tuxedo Sam, Pochacco, Gudetama.
 # Class Type for Hero - 
 # Merchant - 
-# Item - 
+# Item - potion
 # Locations - Forest
-
+#Health: 0 or 100
+#Ability: Gains things for potions when you choose correct decision.
 
 # Introduction: Narrator: "All of your(Hello Kitty) friends have turned evil due to an unknown illness!
 #You need to memorize the paths that your friends are not on, in order to make it to the end of the forest."
@@ -20,39 +21,50 @@
 #Goes back to the beginning of the game.
 
 #If the user chooses left: Print("Safe.") 
-
+#Gains potion solution.
 #Asks question 2: "Do you want to go left or right?"
 
 #If user goes left: 
 # Faces Mimmy: Gets infected and dies. Print("Game Over")
 #Goes back to the beginning of the game.
 #If user goes right:Print("Safe.") 
+#Gains solute for potion.
 
 #Asks question 3: "Do you want to go left or right?"
 
 #If user goes right:faces Pompompurin: Gets infected and dies. Print("Game Over")
 #Goes back to the beginning of the game.
 #If the user goes left: Print("Safe")
+#Gains fairy dust.
 
 #Asks question 4: "Do you want to go left or right?"
 
 #If the user goes right: print("safe")
+#Gains Magic Mixer
 #If the user goes left: 
 #Faces Keroppi: Gets infected and dies. Print("Game over.")
 
 #Asks question 5: "Do you want to go left or righ?"
 #If the user goes right: print("Safe")
+#Gains Magic bottle for potion
 #If the user goes left:
 #Faces Kuromi: Gets infected and dies. Print ("Game over.")
 
 #Asks question 6: "Do you want to go left or right?"
 #If the user goes right: Faces Tuxedo Sam gets infected and dies. Print("Game over")
 #If the user goes left: Print("Safe")
+#Gains Magic Map of the forest.
 
 #Asks question 7: "Do you want to go left or right?"
 #If the user goes right:Print("Safe")
+#Gains Magic rocks.
 #If the user goes left: Faces Pochacco gets infected and dies. Print("Game over")
 
 #Asks question 8: "Do you want to go left or right?"
 #If the user goes right:Faces Gudetama gets infected and dies. Print("Game Over")
 #If the user goes left: Print("Safe")
+#Gains the cork for potion bottle.
+
+#PRINT CONGRADULATIONS.
+# Narrator: "You have obtained all neccesary materials to save your friend."
+#Narrator: You poured the solution 
