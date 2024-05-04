cbaee1e37122ae6e7210d43fbbbef7434840822d
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-03 22:29:09 -0400

    Update middle.py

--- middle.py
+++ middle.py
@@ -5,27 +5,35 @@ right = "<"
 question = None
 left = ">"
 
-def attack():
-    if character < enemy:
-        return
-
-def parts():
-    for i in range(4): 
-        question = input("Do you want to go left or right?")
-        if question == "left":
-            print("Safe")
-        elif question == "right":
-            attack
-            print("Game Over")
+def middle():
+    def attack():
+        if character < enemy:
             return
-        
-        while question == "left":
+
+    def parts():
+        for i in range(4): 
             question = input("Do you want to go left or right?")
-            if question == "right":
+            if question == "left":
                 print("Safe")
-            elif question == "left":
+            elif question == "right":
                 attack
                 print("Game Over")
                 return
+        
+            while question == "left":
+                question = input("Do you want to go left or right?")
+                if question == "right":
+                    print("Safe")
+                elif question == "left":
+                    attack
+                    print("Game Over")
+                    return 
 
-parts()   
+        question = input("Are you ready to see what you got?")
+        if question == "Yes":
+            print("Yay! You've made it! Take a cork to get a potion bottle!")
+        elif question == "No":
+            print("Sorry, but I'm afraid this will leave you as incomplete?")
+    parts()
+
+middle()
