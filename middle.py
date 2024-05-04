70e6f986c66bb7c1ca549bd57bbdaba309fda2c5
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-04 16:56:53 -0400

    Update middle.py

--- middle.py
+++ middle.py
@@ -1,39 +1,32 @@
-from sanrio_characters import Character
-from sanrio_characters import Enemies
+import intro
 
 right = "<"
 question = None
 left = ">"
 
 def middle():
-    def attack():
-        if Character < Enemies and Enemies > Character:
-            return
+      for i in range(4):
+          question = input("Do you want to go left or right?")
+          if question == "left":
+              print("Safe")
+          elif question == "right":
+              print("Game Over")
+              return
+    
+          while question == "left":
+              question = input("Do you want to go left or right?")
+              if question == "right":
+                  print("Safe")
+              elif question == "left":
+                  print("Game Over")
+                  return
 
-    def parts():
-        for i in range(4): 
-            question = input("Do you want to go left or right?")
-            if question == "left":
-                print("Safe")
-            elif question == "right":
-                attack
-                print("Game Over")
-                return
-        
-            while question == "left":
-                question = input("Do you want to go left or right?")
-                if question == "right":
-                    print("Safe")
-                elif question == "left":
-                    attack
-                    print("Game Over")
-                    return 
 
-        question = input("Are you ready to see what you got?")
-        if question == "Yes":
-            print("Yay! You've made it! Take a cork to get a potion bottle!")
-        elif question == "No":
-            print("Sorry, but I'm afraid this will leave you as incomplete?")
-    parts()
+      question = input("Are you ready to see what you got?")
+      if question == "Yes":
+          print("Yay! You've made it! Take a cork to get a potion bottle!")
+      elif question == "No":
+          print("Sorry, but I'm afraid this will leave you as incomplete?")
 
-middle()
+
+middle() 
