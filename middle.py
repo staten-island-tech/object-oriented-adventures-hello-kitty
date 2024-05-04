f5d5ae4ddeaee8a2c6f57a08f0bfade6966774e8
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-05-04 17:48:26 -0400

    g5thynjuy

--- middle.py
+++ middle.py
@@ -1,4 +1,14 @@
-import intro
+narration = ("You are Hello Kitty. All of your friends have turned evil due to an unknown illness! You need to memorize the paths that your friends are not on, in order to make it to the end of the forest.At the end of the forest you will find a potion that turns all your friends back to normal.")
+start = (input("Do you wish to start a Sanrio memory game? Y/N: "))
+def intro(start):
+    for i in start:
+        if start == "Y":
+            print(narration)
+        elif start == "N":
+            print("Exit terminal.")
+        else:
+            print("Invalid")
+intro(start)
 
 right = "<"
 question = None
@@ -11,7 +21,7 @@ def middle():
               print("Safe")
           elif question == "right":
               print("Game Over")
-              return
+              intro(start)
     
           while question == "left":
               question = input("Do you want to go left or right?")
@@ -19,7 +29,7 @@ def middle():
                   print("Safe")
               elif question == "left":
                   print("Game Over")
-                  return
+                  intro(start)
 
 
       question = input("Are you ready to see what you got?")
