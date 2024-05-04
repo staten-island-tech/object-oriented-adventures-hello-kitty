bcf76ff21a4078fa17d2f7620f7a7faab3e224f0
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-04 19:33:41 -0400

    Update middle.py

--- middle.py
+++ middle.py
@@ -1,14 +1,4 @@
-narration = ("You are Hello Kitty. All of your friends have turned evil due to an unknown illness! You need to memorize the paths that your friends are not on, in order to make it to the end of the forest.At the end of the forest you will find a potion that turns all your friends back to normal.")
-start = (input("Do you wish to start a Sanrio memory game? Y/N: "))
-def intro(start):
-    for i in start:
-        if start == "Y":
-            print(narration)
-        elif start == "N":
-            print("Exit terminal.")
-        else:
-            print("Invalid")
-intro(start)
+import intro
 
 right = "<"
 question = None
@@ -21,22 +11,38 @@ def middle():
               print("Safe")
           elif question == "right":
               print("Game Over")
-              intro(start)
-    
+              again = input("Do you want to play again?: Y or N")
+              if again == "Y":
+                  middle()
+                  return
+              if again == "N":
+                  return
+              else: 
+                  print("Invalid")
+          
           while question == "left":
               question = input("Do you want to go left or right?")
               if question == "right":
                   print("Safe")
               elif question == "left":
                   print("Game Over")
-                  intro(start)
-
-
-      question = input("Are you ready to see what you got?")
-      if question == "Yes":
-          print("Yay! You've made it! Take a cork to get a potion bottle!")
-      elif question == "No":
-          print("Sorry, but I'm afraid this will leave you as incomplete?")
-
+                  again = input("Do you want to play again?: Y or N")
+                  if again == "Y":
+                    middle()
+                    return
+                  if again == "N":
+                      return
+                  else:
+                      print("Invalid")
+      while True:
+          question = input("Are you ready to see what you got?:")
+          if question == "Yes":
+             print("Yay! You've made it! Take a cork to get a potion bottle!")
+             return
+          elif question == "No":
+             print("Sorry, but I'm afraid this will leave you as incomplete?")
+             return
+          else:
+              print("Invalid")
 
 middle() 
