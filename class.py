f7b5f807a8b77c5b909362ccdd7c2efeaccac031
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-05 19:49:28 -0400

    Update class.py

--- class.py
+++ class.py
@@ -5,6 +5,7 @@ from middle import question
 from middle import left
 import intro
 import middle
+import end
 
 class Game:
     def intro(start):   
@@ -20,12 +21,14 @@ class Game:
 
     while start == "Y": 
         def middle():         
-            for i in range(4):
+          for i in range(4):
                 question = input("Do you want to go left or right?:")
                 if question == "left":
-                    print("Safe")
+                    print("Safe, with an addition of a potion solution")
+                    print("Health: +15 gems")
                 elif question == "right":
                     print("Game Over")
+                    print("Health: 0 gems")
                     again = input("Do you want to play again?: (Brings you back to questions) Yes or No:")
                     if again == "Yes":
                         middle()
@@ -41,9 +44,11 @@ class Game:
                 while question == "left":
                     question = input("Do you want to go left or right?:")
                     if question == "right":
-                        print("Safe")
+                        print("Safe, with an addition of a potion solution")
+                        print("Health: +15 gems")
                     elif question == "left":
                         print("Game Over")
+                        print("Health: 0 gems")
                         again = input("Do you want to play again?: (Brings you back to questions) Yes or No:")
                         if again == "Yes":
                             middle()
@@ -55,16 +60,18 @@ class Game:
                     else:
                         print("Invalid")
                         exit()
-            while True:
-                question = input("Are you ready to see what you got?: Yes or No?:")
-                if question == "Yes":
-                    print("Yay! You've made it! Take a cork to get a potion bottle!")
-                    return
-                elif question == "No":
-                    print("Sorry, but I'm afraid this will leave you as incomplete.")
-                    return
-                else:
-                    print("Invalid")
-                    exit()
+          while True:
+            question = input("Are you ready to see what you got?: Yes or No?:")
+            if question == "Yes":
+                print("Yay! You've made it! You have 8 potion solutions, and a cork for your potion bottle.")
+                print("Total Health: 120 gems")
+                return
+            elif question == "No":
+                print("Sorry, but I'm afraid this will leave you as incomplete.")
+                return
+            else:
+                print("Invalid")
+                exit()
         middle()
         exit()
+
