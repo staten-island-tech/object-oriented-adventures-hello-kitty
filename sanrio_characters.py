567b06b8f33c52aa744f79a4fe453ac045e8a018
olamideo123 <olamideo12@nycstudents.net>
2024-05-06 12:10:03 -0400

    nn

--- sanrio_characters.py
+++ sanrio_characters.py
@@ -6,18 +6,32 @@ character = [{
 
 Enemies = [{
     'name' : "My Melody",
-    'skill' : "infection",
+    'skill' : "infection",  #Hello Kitty has to deafeat her in a handstand competition
     'type' : "enemy",
 },{
-    'name' : "Mimmy",
+    'name' : "Mimmy",           #Hello Kitty has to deaft her in a karate fight
     'skill' : "infection",
     'type'  : "enemy",
 }, {
-    'name' : "Pompompurin",
+    'name' : "Pompompurin",     #Hello Kitty has to defeat the enemy in a bubblegum blowing competition
     'skill' : "infection",
     'type' : "enemy",
 }, {
     'name' : "Keroppi",
-    'skill' : "infection",
+    'skill' : "infection",     #Hello Kitty has to dfeat the ennemy in a cooking competition
     'type' : "enemy",
-}]
+},  {
+    'name': "Kuromi", 
+    'skill': 
+}
+
+
+
+
+
+
+
+
+
+] 
+
