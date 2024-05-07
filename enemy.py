272418b6959a57034b43b260df4234442e434022
olamideo123 <olamideo12@nycstudents.net>
2024-05-07 12:28:23 -0400

    nn

--- enemy.py
+++
@@ -1,1 +0,0 @@
-enemies = ["My Melody", "Mimmy", "Pompompurin", "Keroppi", "Kuromi", "Tuxedo Sam", "Pochacco", "Gudetama"]
---
+++ friend list.py
@@ -0,0 +1,15 @@
+
+friends = [{
+
+    'name' : "Keroppi",           #Afsheen do it up to here
+    'skill' : "cooking",     #Hello Kitty has to has a cooking competition
+    'type' : "friend",
+}, {
+    'name': "Tuxedo Sam",                 #they have a competitoon on how can tie 
+    'skill': "making tie",
+    'type': "friend",
+}] 
+
+friends = ["Keroppi", "Tuxedo Sam"] 
+
+
