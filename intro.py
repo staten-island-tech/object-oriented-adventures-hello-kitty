b6d202e37e65f42260b815238653d2ce4f2b1463
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-09 16:22:11 -0400

    Update intro.py

--- intro.py
+++ intro.py
@@ -9,32 +9,7 @@ def intro(start):
             print("Exit Game. We hope to see you again!")
         else:
             print("Invalid")
+            exit()
 intro(start)
 =======
-from enemy import create_main
-narration = ("You are Hello Kitty. All of your friends have turned evil due to an unknown illness! You need to memorize the paths that your friends are not on, in order to make it to the end of the forest.At the end of the forest you will find a potion that turns all your friends back to normal.")
-add_heros = 'Y'
-start = (input("Do you wish to start a Sanrio memory game? Y/N: "))
 
-def intro(start):
-    for i in start:
-        if start == "Y":
-            print(narration)
-        elif start == "N":
-            print("Exit terminal.")
-        else:
-            print("Invalid")
-
-    while add_heros == "Y":
-     user_request = input("Do you want to add the main character? Y/N: ")
-     if user_request == "Y":
-      name = "Hello Kitty"
-      skill = "Battling"
-      type = "Hero"
-      main = "Main character"
-      create_main(name, skill, type, main)
-     elif user_request == "N":
-       break
-     else:
-       print("Invalid")
->>>>>>> Stashed changes
