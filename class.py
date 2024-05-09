25dd5b310c4d43f7b7b1687f32b6c4f3f59f7f86
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-09 11:57:51 -0400

    .

--- class.py
+++ class.py
@@ -2,20 +2,92 @@ narration = ("You are Hello Kitty. All of your friends have turned evil due to a
 start = (input("Do you wish to start a Sanrio memory game? Y/N: "))
 
 class Game:
+<<<<<<< Updated upstream
     def intro():
         for i in start:
             start
             if start == "Y":
                 print(narrator)
+=======
+    def intro(start):
+        for i in start:
+            start
+            if start == "Y":
+                print(narration)
+>>>>>>> Stashed changes
             elif start == "N":
                 print("Exit terminal.")
             else:
                 print("Invalid")
+<<<<<<< Updated upstream
                 exit()
     intro(start)
 
     while start == "Y": 
       def middle(): 
+=======
+
+    intro(start)
+
+    while start == "Y": 
+        def middle():         
+          for i in range(4):
+                question = input("Do you want to go left or right?:")
+                if question == "left":
+                    print("Safe, with an addition of a potion solution")
+                    print("Health: +15 gems")
+                elif question == "right":
+                    print("Game Over")
+                    print("Health: 0 gems")
+                    again = input("Do you want to play again?: (Brings you back to questions) Yes or No:")
+                    if again == "Yes":
+                        middle()
+                        return
+                    if again == "No":
+                        return
+                    else:
+                        print("Invalid")
+                else:
+                    print("Invalid")
+                    exit()
+        
+                while question == "left":
+                    question = input("Do you want to go left or right?:")
+                    if question == "right":
+                        print("Safe, with an addition of a potion solution")
+                        print("Health: +15 gems")
+                    elif question == "left":
+                        print("Game Over")
+                        print("Health: 0 gems")
+                        again = input("Do you want to play again?: (Brings you back to questions) Yes or No:")
+                        if again == "Yes":
+                            middle()
+                            return
+                        if again == "No":
+                            return
+                        else:
+                            print("Invalid")
+                    else:
+                        print("Invalid")
+                        exit()
+          while True:
+            question = input("Are you ready to see what you got?: Yes or No?:")
+            if question == "Yes":
+                print("Yay! You've made it! You have 8 potion solutions, and a cork for your potion bottle.")
+                print("Total Health: 120 gems")
+                return
+            elif question == "No":
+                print("Sorry, but I'm afraid this will leave you as incomplete.")
+                return
+            else:
+                print("Invalid")
+                exit()
+        middle()
+        exit()
+    
+    while start == "Y": 
+      def middle():
+>>>>>>> Stashed changes
          question = input("Do you want to go left or right?:")
          if question == "left":
              print("Safe, with an addition of a potion solution")
@@ -183,6 +255,7 @@ class Game:
                 exit()
             elif question == "No":
                 print("Sorry, but I'm afraid this will leave you as incomplete.")
+<<<<<<< Updated upstream
                 return
             else:
                 print("Invalid")
@@ -191,3 +264,10 @@ class Game:
     
 
 
+=======
+                exit()
+            else:
+                print("Invalid")
+                exit()
+      middle()
+>>>>>>> Stashed changes
--- intro.py
+++ intro.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 narrator = ("You are Hello Kitty. All of your friends have turned evil due to an unknown illness! You need to compete with your friends in competitions inorder to obtain items for the potion.At the end of the forest you will find a potion that turns all your friends back to normal.")
 start = (input("Do you wish to start a Sanrio memory game? Y/N: "))
 def intro(start):
@@ -9,3 +10,31 @@ def intro(start):
         else:
             print("Invalid")
 intro(start)
+=======
+from enemy import create_main
+narration = ("You are Hello Kitty. All of your friends have turned evil due to an unknown illness! You need to memorize the paths that your friends are not on, in order to make it to the end of the forest.At the end of the forest you will find a potion that turns all your friends back to normal.")
+add_heros = 'Y'
+start = (input("Do you wish to start a Sanrio memory game? Y/N: "))
+
+def intro(start):
+    for i in start:
+        if start == "Y":
+            print(narration)
+        elif start == "N":
+            print("Exit terminal.")
+        else:
+            print("Invalid")
+
+    while add_heros == "Y":
+     user_request = input("Do you want to add the main character? Y/N: ")
+     if user_request == "Y":
+      name = "Hello Kitty"
+      skill = "Battling"
+      type = "Hero"
+      main = "Main character"
+      create_main(name, skill, type, main)
+     elif user_request == "N":
+       break
+     else:
+       print("Invalid")
+>>>>>>> Stashed changes
