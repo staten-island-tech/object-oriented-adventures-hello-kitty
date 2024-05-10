7f65e110654eec9528c4f5ab3f2f8d072408c729
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-05-10 12:29:59 -0400

    sgresg

--- intro.py
+++ intro.py
@@ -1,4 +1,3 @@
-import classes
 
 narrator = ("Hello. You are Hello Kitty. All of your friends have turned evil due to an unknown illness! You need to memorize the paths that your friends are not on, in order to make it to the end of the forest.At the end of the forest you will find a potion that turns all your friends back to normal.")
 start = (input("Do you wish to start a Sanrio memory game? Y/N: "))
