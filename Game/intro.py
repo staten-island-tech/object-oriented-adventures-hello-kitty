719142a8819ba5d8fd5087094928512002ebb40a
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-15 12:16:37 -0400

    .

--- Game/intro.py
+++ Game/intro.py
@@ -1,6 +1,6 @@
-from Game.classes import create_main
-from Game.classes import create_enemies5
-from Game.classes import create_friends
+from classes import create_main
+from classes import create_enemies5
+from classes import create_friends
 
 narration = ("Hello and welcome to the Hello Kitty Potion Game! You are Hello Kitty, and all of your friends have turned evil due to an unknown illness. You will need to compete with your friends in various competitions in order to obtain items for a blueberry potion. At the end of the forest you will find a potion that turns all your friends back to normal.")
 start = (input("Do you wish to start a Sanrio memory game? Y/N: "))
