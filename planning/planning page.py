ab90d8312e263d2683da88f4e26249ebd8fbe7ee
olamideo123 <olamideo12@nycstudents.net>
2024-05-17 12:13:06 -0400

    nn

---
+++ planning/planning page.py
@@ -0,0 +1,83 @@
+# Hero - Hello Kitty
+# Enemy - Friends(original) 
+#Enemies (8): My Melody, Mimmy, Pompompurin, Keroppi, Kuromi, Tuxedo Sam, Pochacco, Gudetama.
+# Class Type for Hero - 
+# Merchant - 
+# Item - potion
+# Locations - Forest
+#Health: 0 or 100
+#Ability: Gains things for potions when you choose correct decision.
+
+# Introduction: Narrator: "All of your(Hello Kitty) friends have turned evil due to an unknown illness!
+#You need to memorize the paths that your friends are not on, in order to make it to the end of the forest."
+#At the end of the forest you will find a potion that turns all your friends back to normal."
+
+
+# Asks question1: Do you want to go left or right?
+
+# If the user goes right:
+# Faces My Melody: Gets infected and dies. Print("Game Over")                             left  Q1
+#Goes back to the beginning of the game.
+
+#If the user chooses left: Print("Safe.") 
+#Gains Magic Map of the forest. 
+#Melody:"You can't beat me"
+
+
+#Asks question 2: "Do you want to go left or right?"
+
+#If user goes left: 
+# Faces Mimmy: Gets infected and dies. Print("Game Over")                               right Q2
+#Goes back to the beginning of the game.
+#If user goes right:Print("Safe.") 
+#Gains a magic bag with unlimited space. 
+#Mimmy:"I'm going to beat you"
+
+
+#Asks question 3: "Do you want to go left or right?"
+
+#If user goes right:faces Pompompurin: Gets infected and dies. Print("Game Over")
+#Goes back to the beginning of the game.                                                    left Q3
+#If the user goes left: Print("Safe")
+#Gains recipe book for potions. 
+#"You can't win against me"
+
+#Asks question 4: "Do you want to go left or right?"
+
+#If the user goes left: print("safe")                                                  left Q4
+#Gains bottle for potion
+#Keroppi 
+#""
+
+#Asks question 5: "Do you want to go left or righ?"
+#If the user goes right: print("Safe")
+#Gains fairy dust.
+#If the user goes left:                                                               right Q5
+#Faces Kuromi: Gets infected and dies. Print ("Game over.")
+
+#Asks question 6: "Do you want to go left or right?"
+#If the user goes right: Print("Safe")                                                     right Q6
+#Gains potion solute.
+#Tuxedo Sam
+
+#Asks question 7: "Do you want to go left or right?"
+#If the user goes right:Print("Safe")                                                   right  Q7
+#Gains potion soluvent and unicorn dust.
+#If the user goes left: Faces Pochacco gets infected and dies. Print("Game over")
+
+#Asks question 8: "Do you want to go left or right?"
+#If the user goes right:Faces Gudetama gets infected and dies. Print("Game Over")            left Q8
+#If the user goes left: Print("Safe")
+#Gains the cork for a potion bottle.
+
+#END PRINTS.
+# Narrator: "You have obtained all the neccesary materials to save your friends."
+#Narrator: After you opened your bag and took out the recipe book and turned to the page about curing evil people 
+#You created a solution with the solute and soluvent then added the unicorn hair, and fairy dust into the magic bottle. Then it was throughly shook for 10 minutes
+#after you give Gudetama a sip of the majic potion you and him went back and gave all of your friends a sip
+#Finally, the magic potion is poured on to the magic map and cures all of your friends from the infection.
+
+#Maybe use while loop? 
+#Do you want to play again Y/N
+#If yes restart game. If No print end screen.
+# The End.  
