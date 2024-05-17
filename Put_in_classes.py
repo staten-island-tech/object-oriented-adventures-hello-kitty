3938b6f8f0cebfb7d7249e369ea9b20a6d9e5f75
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-05-17 11:59:06 -0400

    Update Put_in_classes.py

---
+++ Put_in_classes.py
@@ -0,0 +1,21 @@
+import uuid
+
+class middle:
+    def __init__(self, next):
+        self.b = []
+        self.next = next
+    def part1():
+        next = input("Welcome! You've entered Level 1! Want to see your first opponent? Y or N:")
+    for i in range(1):
+        if next == "Y":
+          name = "My Melody"
+          skill = "Handstand endurance"
+          type = "Enemy"
+          status2= "Infected"
+          health = 0
+          print(name, skill, type, status2, health)
+          print("You've come across My Melody.")
+        else:
+          print("Invalid")
+          exit()
+          
