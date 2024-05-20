e3bfd427873257e625e80b4d59355f42054bf1d2
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-20 12:22:47 -0400

    .

---
+++ Game/middle.py
@@ -0,0 +1,12 @@
+import characters
+
+def middle():
+    characters.part1
+    characters.part2
+    characters.part3
+    characters.part4
+    characters.part5
+    characters.part6
+    characters.part7
+    characters.part8
+middle()
