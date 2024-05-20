4ffc5d087798a8158f66cbc4bdbaf39bc1c85def
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-20 12:11:30 -0400

    Create full_game_test.py

---
+++ Game/full_game_test.py
@@ -0,0 +1,16 @@
+import characters
+
+class Game:
+    def intro():
+        characters.introduction
+
+    def middle(intro):
+        characters.part1
+        characters.part2
+        characters.part3
+        characters.part4
+        characters.part5
+        characters.part6
+        characters.part7
+        characters.part8
+    middle(intro)
