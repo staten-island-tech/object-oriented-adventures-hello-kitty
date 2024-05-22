562e7c93b916eb62a06a72e2513a3438a92eec3a
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-21 20:50:12 -0400

    .

--- Game/full_game_test.py
+++ Game/full_game_test.py
@@ -2,20 +2,20 @@ import characters
 
 class Game:
     def intro():
-        characters.introduction
+        characters.introduction()
 
     def middle():
-        characters.part1
-        characters.part2
-        characters.part3
-        characters.part4
-        characters.part5
-        characters.part6
-        characters.part7
-        characters.part8
+        characters.part1()
+        characters.part2()
+        characters.part3()
+        characters.part4()
+        characters.part5()
+        characters.part6()
+        characters.part7()
+        characters.part8()
     middle()
 
     def end():
-        characters.conclusion
-        characters.restart
+        characters.conclusion()
+        characters.restart()
     end()
