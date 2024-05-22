f924172aa42817aec294ba89053ad559f21634d5
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-21 20:57:28 -0400

    .

--- Game/characters2.py
+++ Game/characters2.py
@@ -1,6 +1,6 @@
 def go_on():
     level_up = input("Do you want to proceed to the next level? Y or N:")
-    for i in range(1):
+    for i in level_up:
         if level_up == "Y":
             print("Great! Let's move on!")
             return
