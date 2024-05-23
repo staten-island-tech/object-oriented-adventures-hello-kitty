7d5573e3628bf6a5ca5bf45675c1ddebbf971f87
HongzhengWu51 <HOngzhengw2789@nycstudents.net>
2024-05-23 12:19:23 -0400

    b

--- Game/characters.py
+++ Game/characters.py
@@ -25,7 +25,7 @@ def introduction():
              print("Invalid")
              exit()
        while user_request == "Y":
-         move_on = input("Enter Next:")
+         move_on.lower = input("Enter Next:")
          for i in range(1):
              if move_on == "Next":
                  print("Great! Welcome Hello Kitty! You sure are missing your friends.")
@@ -34,7 +34,7 @@ def introduction():
                  print("invalid")
                  exit()
   
-         while move_on == "Next":
+         while move_on.lower == "Next":
              request = input("Do you want to add enemies?Y/N: ")
              for i in range(1):
                  if request == "Y":
