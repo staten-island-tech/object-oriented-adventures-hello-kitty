8a2dcafd10ac06335488a171426a996b6cdabc4f
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-29 17:42:22 -0400

    .

--- Game/characters.py
+++ Game/characters.py
@@ -99,7 +99,7 @@ def part1():
         print("Invalid")
         again = input("Do you want to start all over again?: Yes or No:")
         if again == "Yes":
-            restart()
+            restart
         else:
             print("Good Bye!")
             exit()
