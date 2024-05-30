f8043b3ca188fd7f47edf4cd3050391d8c10ed49
afsheenbahri <158204148+afsheenbahri@users.noreply.github.com>
2024-05-30 12:04:18 -0400

    Update characters.py

--- Game/characters.py
+++ Game/characters.py
@@ -1,7 +1,6 @@
 narration = ("Hello and welcome to the Hello Kitty Potion Game! You are Hello Kitty, and all of your friends have turned evil due to an unknown illness. You will need to compete with your friends in various competitions in order to obtain items for a blueberry potion. At the end of the forest you will find a potion that turns all your friends back to normal.")
 import classes
 import characters2
-import characters
 
 def introduction():
  start = (input("Do you wish to start a Sanrio memory game? Y/N: "))
